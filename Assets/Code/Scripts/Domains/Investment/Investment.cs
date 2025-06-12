d8b8e6feed4b3f27cb56c801d6c2515d68a215ad
mschpr <m.scheper@stud.phwt.de>
2025-06-12 11:54:13 +0200

    CETERRA-31 Added method to deserialize investments from JSON list with test

--- Assets/Code/Scripts/Domains/Investment/Investment.cs
+++ Assets/Code/Scripts/Domains/Investment/Investment.cs
@@ -1,13 +1,18 @@
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class Investment {
     protected int id;
     protected string displayName;
     protected string description;
-    protected int goal;
+    protected int moneyGoal;
+    protected int resourceGoal;
+    protected int moneyCurrentValue = 0;
     protected bool fulfilled = false;
-    protected int currentValue = 0;
     protected (InvestmentDisplay, InvestmentModifier, int) investmentType;
+    protected int cooldown;
 
     public int Id {
         get; set;
@@ -21,7 +26,11 @@ public class Investment {
         get; set;
     }
 
-    public int Goal {
+    public int MoneyGoal {
+        get; set;
+    }
+
+    public int ResourceGoal {
         get; set;
     }
 
@@ -29,16 +38,20 @@ public class Investment {
         get; set;
     }
 
-    public int CurrentValue {
+    public int Cooldown {
+        get; set;
+    }
+
+    public int MoneyCurrentValue {
         get {
-            return this.currentValue;
+            return this.moneyCurrentValue;
         }
         set {
-            if (value < 0 || value > this.Goal) {
-                throw new Exception("Value must be between 0 and " + this.Goal);
+            if (value < 0 || value > this.MoneyGoal) {
+                throw new Exception("Value must be between 0 and " + this.MoneyGoal);
             }
             else {
-                this.currentValue = value;
+                this.moneyCurrentValue = value;
             }
         }
     }
@@ -47,22 +60,40 @@ public class Investment {
         get; set;
     }
 
-    public Investment(int id, string displayName, string description, int goal) {
+    public Investment(int id, string displayName, string description, int moneyGoal, int resourceGoal, InvestmentDisplay investmentDisplay, InvestmentModifier investmentModifier, int investmentTypeInt, int cooldown) {
         this.Id = id;
         this.DisplayName = displayName;
         this.Description = description;
-        this.Goal = goal;
+        this.MoneyGoal = moneyGoal;
+        this.ResourceGoal = resourceGoal;
+        this.InvestmentType = (investmentDisplay, investmentModifier, investmentTypeInt);
+        this.Cooldown = cooldown;
     }
 
-    public void Invest(int add) {
-        if (this.CurrentValue + add > this.Goal) {
+    public void InvestMoney(int add) {
+        if (this.MoneyCurrentValue + add > this.MoneyGoal) {
             throw new Exception("Goal must not be exceeded");
         }
         else {
-            this.CurrentValue += add;
-            if (this.CurrentValue == this.Goal) {
+            this.MoneyCurrentValue += add;
+            if (this.MoneyCurrentValue == this.MoneyGoal) {
                 this.Fulfilled = true;
             }
         }
+    }
+
+    public void FulfillInvestment(ResourceDisplay resourceDisplay) {
+        if (this.MoneyGoal == this.MoneyCurrentValue && this.resourceGoal <= resourceDisplay.CurrentValue) {
+            this.Fulfilled = true;
+        }
+        else {
+            throw new Exception("Missing money or resources to fulfill investment");
+        }
+    }
+
+    public static List<Investment> LoadInvestmentsFromResources() {
+        string jsonText = File.ReadAllText("Assets/Resources/Domains/Investment/InvestmentList.json");
+        List<Investment> investmentList = JsonConvert.DeserializeObject<List<Investment>>(jsonText);
+        return investmentList;
     }
 }
