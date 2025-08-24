981e80c4ab0f9a2d4ba21d7703edbb9f8194b2c6
Mattes <github.mattes@ossenbeck.dev>
2025-08-24 22:49:33 +0200

    Correct unintended FieldType changes [https://ceterispartybus.atlassian.net/browse/CETERRA-36]

--- Assets/Code/Scripts/Domains/Board/Field/EventField.cs
+++ Assets/Code/Scripts/Domains/Board/Field/EventField.cs
@@ -3,7 +3,7 @@ using UnityEngine.Splines;
 
 public class EventField : Field {
     public EventField(int id, int splineId, SplineKnotIndex splineKnotIndex, Vector3 position, Transform transform)
-        : base(id, splineId, FieldType.NORMAL, splineKnotIndex, position, transform) {
+        : base(id, splineId, FieldType.EVENT, splineKnotIndex, position, transform) {
     }
 
     public override void Invoke(BoardPlayer player) {
--- Assets/Code/Scripts/Domains/Board/Field/QuestionField.cs
+++ Assets/Code/Scripts/Domains/Board/Field/QuestionField.cs
@@ -3,7 +3,7 @@ using UnityEngine.Splines;
 
 public class QuestionField : Field {
     public QuestionField(int id, int splineId, SplineKnotIndex splineKnotIndex, Vector3 position, Transform transform)
-        : base(id, splineId, FieldType.NORMAL, splineKnotIndex, position, transform) {
+        : base(id, splineId, FieldType.QUESTION, splineKnotIndex, position, transform) {
     }
 
     public override void Invoke(BoardPlayer player) {
