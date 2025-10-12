232b27246a00940051e02bfe17734513fe22140d
Mattes <github.mattes@ossenbeck.dev>
2025-10-13 01:50:21 +0200

    Use sprites for turn ui buttons [https://ceterispartybus.atlassian.net/browse/CETERRA-68]

--- Assets/Code/Scripts/UI/Board/CurrentTurnManager.cs
+++ Assets/Code/Scripts/UI/Board/CurrentTurnManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using DG.Tweening;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public class CurrentTurnManager : NetworkedSingleton<CurrentTurnManager> {
