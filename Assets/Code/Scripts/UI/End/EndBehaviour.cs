e374a1daa101f13bd496ae035e0d02c2fad6ef77
koelker <f.koelker@stud.phwt.de>
2025-10-13 22:01:34 +0200

    Show all player scores on end screen [https://ceterispartybus.atlassian.net/browse/CETERRA-88]

--- Assets/Code/Scripts/UI/End/EndBehaviour.cs
+++ Assets/Code/Scripts/UI/End/EndBehaviour.cs
@@ -1,5 +1,7 @@
 using Mirror;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,23 +11,47 @@ public class EndBehaviour : NetworkBehaviour {
     [ClientCallback]
     void Start() {
         int groupScore = BoardContext.Instance.EvaluateGlobalScore();
-        int playerScore = BoardContext.Instance.GetLocalPlayer().PlayerStats.GetScore();
-        int totalScore = playerScore + groupScore;
+        var localPlayer = BoardContext.Instance.GetLocalPlayer();
+        int localPlayerScore = localPlayer.PlayerStats.GetScore();
+        int localPlayerTotalScore = localPlayerScore + groupScore;
 
-        var boardPlayers = BoardContext.Instance.GetRemotePlayers();
+        var remotePlayers = BoardContext.Instance.GetRemotePlayers();
 
         Label localPlayerScoreLabel = uiDocument.rootVisualElement.Q<Label>("local-player-score");
         Label groupScoreLabel = uiDocument.rootVisualElement.Q<Label>("group-score");
+        VisualElement otherPlayersContainer = uiDocument.rootVisualElement.Q<VisualElement>("other-players-container");
 
-        localPlayerScoreLabel.text = totalScore.ToString();
         groupScoreLabel.text = groupScore.ToString();
+        localPlayerScoreLabel.text = localPlayerTotalScore.ToString();
 
-        Debug.Log($"Group score: {groupScore}");
-        Debug.Log($"Local player score: {playerScore}");
-        Debug.Log($"Total score: {totalScore}");
+        var playerScores = new List<(string name, int totalScore)>();
 
-        foreach (var player in boardPlayers) {
-            Debug.Log($"Player {player.PlayerName} score: {player.PlayerStats.GetScore()}");
+        foreach (var player in remotePlayers) {
+            int playerIndividualScore = player.PlayerStats.GetScore();
+            int playerTotalScore = playerIndividualScore + groupScore;
+            playerScores.Add((player.PlayerName, playerTotalScore));
+        }
+
+        playerScores = playerScores.OrderByDescending(p => p.totalScore).ToList();
+
+        foreach (var (name, totalScore) in playerScores) {
+            VisualElement playerRow = new VisualElement();
+            playerRow.AddToClassList("player-row");
+            playerRow.style.flexDirection = FlexDirection.Row;
+            playerRow.style.fontSize = 60;
+            playerRow.style.marginBottom = 10;
+
+            Label playerNameLabel = new Label(name + ":");
+            playerNameLabel.style.marginRight = 30;
+            playerNameLabel.style.minWidth = 200;
+            playerNameLabel.style.color = new Color(0.9f, 0.9f, 0.9f);
+
+            Label playerScoreLabel = new Label(totalScore.ToString());
+            playerScoreLabel.style.color = new Color(0.5f, 0.8f, 1.0f); // Light blue
+
+            playerRow.Add(playerNameLabel);
+            playerRow.Add(playerScoreLabel);
+            otherPlayersContainer.Add(playerRow);
         }
     }
 }
