28597974834794a15079d53e1eeec97f30028a83
koelker <f.koelker@stud.phwt.de>
2025-10-13 21:52:52 +0200

    Move to Start method in order to prevent timing issues that occur in OnStartClient [https://ceterispartybus.atlassian.net/browse/CETERRA-88]

--- Assets/Code/Scripts/UI/End/EndBehaviour.cs
+++ Assets/Code/Scripts/UI/End/EndBehaviour.cs
@@ -1,19 +1,31 @@
 using Mirror;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class EndBehaviour : NetworkBehaviour {
     [SerializeField] private UIDocument uiDocument;
 
-    public override void OnStartClient() {
+    [ClientCallback]
+    void Start() {
         int groupScore = BoardContext.Instance.EvaluateGlobalScore();
         int playerScore = BoardContext.Instance.GetLocalPlayer().PlayerStats.GetScore();
         int totalScore = playerScore + groupScore;
+
+        var boardPlayers = BoardContext.Instance.GetRemotePlayers();
 
         Label localPlayerScoreLabel = uiDocument.rootVisualElement.Q<Label>("local-player-score");
         Label groupScoreLabel = uiDocument.rootVisualElement.Q<Label>("group-score");
 
         localPlayerScoreLabel.text = totalScore.ToString();
         groupScoreLabel.text = groupScore.ToString();
+
+        Debug.Log($"Group score: {groupScore}");
+        Debug.Log($"Local player score: {playerScore}");
+        Debug.Log($"Total score: {totalScore}");
+
+        foreach (var player in boardPlayers) {
+            Debug.Log($"Player {player.PlayerName} score: {player.PlayerStats.GetScore()}");
+        }
     }
 }
