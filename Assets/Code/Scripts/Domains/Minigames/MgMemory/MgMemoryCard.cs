33a48f5ea7f00cbc5ab7472f446e05caa47fad46
René <rene.froemmel@gmx.de>
2025-11-02 15:16:06 +0100

    add flip animation

--- Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryCard.cs
+++ Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryCard.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Mirror;
 using System;
 using UnityEngine;
@@ -6,12 +7,14 @@ using UnityEngine.UI;
 public class Card : NetworkBehaviour {
     [SerializeField] private Image cardImage;
     [SerializeField] private Sprite hiddenIconSprite;
+    [SerializeField] private float flipDuration = 0.3f;
     private Sprite iconSprite;
     public Sprite GetIconSprite => iconSprite;
 
     public MgMemoryGameController memoryController;
 
     private bool isSelected;
+    private bool isAnimating;
 
     public bool IsSelected => isSelected;
 
@@ -29,12 +32,35 @@ public class Card : NetworkBehaviour {
     }
 
     public void Show() {
-        cardImage.sprite = iconSprite;
-        isSelected = true;
+        if (isAnimating) {
+            return;
+        }
+
+        StartFlipAnimation(iconSprite, true);
     }
 
     public void Hide() {
-        cardImage.sprite = hiddenIconSprite;
-        isSelected = false;
+        if (isAnimating) {
+            return;
+        }
+
+        StartFlipAnimation(hiddenIconSprite, false);
+    }
+
+    private void StartFlipAnimation(Sprite targetSprite, bool willBeSelected) {
+        isAnimating = true;
+
+        cardImage.transform.DORotate(new Vector3(0, 90, 0), flipDuration / 2f)
+            .SetEase(Ease.InQuad)
+            .OnComplete(() => {
+                cardImage.sprite = targetSprite;
+                isSelected = willBeSelected;
+
+                cardImage.transform.DORotate(Vector3.zero, flipDuration / 2f)
+                    .SetEase(Ease.OutQuad)
+                    .OnComplete(() => {
+                        isAnimating = false;
+                    });
+            });
     }
 }
