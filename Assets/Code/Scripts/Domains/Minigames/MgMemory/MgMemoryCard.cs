89deb8063cd0cb69f6673b0578631a7d029a53c2
René <rene.froemmel@gmx.de>
2025-11-02 15:06:23 +0100

    implement memory (1/2)

--- Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryCard.cs
+++ Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryCard.cs
@@ -1,40 +1,86 @@
+using DG.Tweening;
 using Mirror;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Card : NetworkBehaviour {
-    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Image cardImage;
+    [SerializeField] private Sprite hiddenIconSprite;
+    [SerializeField] private float flipDuration = 0.3f; // Dauer der Flip-Animation
+    private Sprite iconSprite;
+    public Sprite GetIconSprite => iconSprite;
 
-    public Sprite hiddenIconSprite;
-    public Sprite iconSprite;
+    public MgMemoryGameController memoryController;
+    
+    [SyncVar] private int cardIndex = -1; // Eindeutige ID für Netzwerk-Synchronisation
+    public int CardIndex => cardIndex;
 
-    public bool isSelected;
+    private bool isSelected;
+    private bool isAnimating; // Verhindert mehrfache Animationen
+
+    public bool IsSelected => isSelected;
+
     private void Awake() {
-        spriteRenderer ??= GetComponent<SpriteRenderer>();
+        cardImage ??= GetComponent<Image>();
         Hide();
     }
 
     public void OnCardClicked() {
-        Debug.Log($"Card clicked! IsSelected: {isSelected}, IconSprite: {iconSprite?.name}");
+        // Command direkt an GameController senden
+        memoryController.CmdSelectCard(cardIndex);
+    }
 
-        if (isSelected) {
-            Hide();
-        }
-        else {
-            Show();
-        }
+    public void SetCardIndex(int index) {
+        cardIndex = index;
     }
 
     public void SetIconSprite(Sprite sprite) {
         iconSprite = sprite;
     }
 
-    public void Show() {
-        spriteRenderer.sprite = iconSprite;
-        isSelected = true;
+    // Lokale Show-Methode ohne Netzwerk (wird über RPC aufgerufen)
+    public void ShowLocal() {
+        if (isAnimating) {
+            return;
+        }
+        
+        StartFlipAnimation(iconSprite, true);
     }
 
+    // Lokale Hide-Methode ohne Netzwerk (wird über RPC aufgerufen)
+    public void HideLocal() {
+        if (isAnimating) {
+            return;
+        }
+        
+        StartFlipAnimation(hiddenIconSprite, false);
+    }
+
+    // Deprecated - nur für Kompatibilität
+    public void Show() {
+        ShowLocal();
+    }
+
+    // Deprecated - nur für Kompatibilität  
     public void Hide() {
-        spriteRenderer.sprite = hiddenIconSprite;
-        isSelected = false;
+        HideLocal();
+    }
+
+    private void StartFlipAnimation(Sprite targetSprite, bool willBeSelected) {
+        isAnimating = true;
+
+        cardImage.transform.DORotate(new Vector3(0, 90, 0), flipDuration / 2f)
+            .SetEase(Ease.InQuad)
+            .OnComplete(() => {
+                cardImage.sprite = targetSprite;
+                isSelected = willBeSelected;
+
+                cardImage.transform.DORotate(Vector3.zero, flipDuration / 2f)
+                    .SetEase(Ease.OutQuad)
+                    .OnComplete(() => {
+                        isAnimating = false;
+                    });
+            });
     }
 }
--- Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryContext.cs
+++ Assets/Code/Scripts/Domains/Minigames/MgMemory/MgMemoryContext.cs
@@ -11,7 +11,7 @@ public class MgMemoryContext : NetworkedSingleton<MgMemoryContext> {
     [Header("Minigame Settings")]
     [SerializeField] private float GameDuration;
     [SerializeField] private float scoreboardDuration;
-    [SerializeField] private string memoryFileName;
+    //[SerializeField] private MgMemoryGameController memoryGameController;
 
     private float countdownTimer;
     private Coroutine countdownCoroutine;
@@ -25,13 +25,34 @@ public class MgMemoryContext : NetworkedSingleton<MgMemoryContext> {
     }
 
     private IEnumerator MemoryRoutine() {
+        yield return new WaitForSeconds(0.5f);
+        StartMemory();
         yield return new WaitForSeconds(GameDuration);
 
+        RpcClearMemoryOnClients();
+        yield return new WaitForSeconds(0.1f);
         MgMemoryController.Instance.ShowScoreboard();
         yield return new WaitForSeconds(scoreboardDuration);
 
         StopMemory();
         GameManager.Singleton.EndMinigame();
+    }
+
+    [Server]
+    private void StartMemory() {
+        var randomSeed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+
+        RpcInitializeCardsOnClients(randomSeed);
+    }
+
+    [ClientRpc]
+    private void RpcInitializeCardsOnClients(int randomSeed) {
+        MgMemoryGameController.Instance.InitializeCardsOnClients(randomSeed);
+    }
+
+    [ClientRpc]
+    private void RpcClearMemoryOnClients() {
+        MgMemoryGameController.Instance.ClearMemory();
     }
 
     [Server]
